7e98c005d15cc01c419b773afa09c7b5c3d3376f
demo-burner-account <128810511+demo-burner-account@users.noreply.github.com>
2023-04-03 12:48:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -16,22 +16,7 @@
                 <th>Id</th>
                 <th>Message</th>
             </tr>
-            <?php
-            $user = 'root';
-            $pass = 'example';
-
-            try {
-                $dbh = new PDO('mysql:host=mysql-service.default.svc;port=3306;dbname=app', $user, $pass);
-                foreach ($dbh->query('SELECT * from message') as $row) {
-                    $html = "<tr><td>${row['id']}</td><td>${row['message']}</td></tr>";
-                    echo $html;
-                }
-                $dbh = null;
-            } catch (PDOException $e) {
-                print "Error!: " . $e->getMessage() . "<br/>";
-                die();
-            }
-            ?>
+            
         </table>
     </div>
 </body>
